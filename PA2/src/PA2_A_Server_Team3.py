d84beb0cf41f6911f12d33f5d6ab8d0fe7fe9a31
Andi <127003684+and1cam@users.noreply.github.com>
2023-05-04 19:52:19 -0700

    Update PA2_A_Server_Team3.py

--- PA2/src/PA2_A_Server_Team3.py
+++ PA2/src/PA2_A_Server_Team3.py
@@ -1,4 +1,3 @@
-#!env python
 """UDP server for CST311 Programming Assignment 2"""
 __author__ = "Team 3"
 __credits__ = [
@@ -19,26 +18,36 @@ log.setLevel(logging.DEBUG)
 SERVER_PORT = 12000
 
 def main():
+  
   # Create a UDP socket
   # Notice the use of SOCK_DGRAM for UDP packets
   serverSocket = s.socket(s.AF_INET, s.SOCK_DGRAM)
+  
   # Assign IP address and port number to socket
   serverSocket.bind(('', SERVER_PORT))
+  
   log.info("Waiting for client.")
+  
   pingnum = 0
+  
   while True:
+    
     # Count the pings received
     pingnum += 1
+    
     # Generate random number in the range of 0 to 10
     rand = random.randint(0, 10)
+    
     # Receive the client packet along with the
     # address it is coming from
     message, address = serverSocket.recvfrom(1024)
+    
     # If rand is less is than 4, and this not the
     # first "ping" of a group of 10, consider the
     # packet lost and do not respond
     if rand < 4 and pingnum % 10 != 1:
       continue
+      
     # Otherwise, the server responds
     serverSocket.sendto(message, address)
 
