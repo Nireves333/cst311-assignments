fac29941aa94901d3aae1eb1072283278d8c57a9
Andi <127003684+and1cam@users.noreply.github.com>
2023-05-04 23:21:20 -0700

    Update PA2_A_Client_Team3.py

--- PA2/src/PA2_A_Client_Team3.py
+++ PA2/src/PA2_A_Client_Team3.py
@@ -1,4 +1,3 @@
-#!env python
 """UDP client for CST311 Programming Assignment 2"""
 __author__ = "Team 3"
 __credits__ = [
@@ -8,20 +7,70 @@ __credits__ = [
   "Severin Light"
 ]
 
-# Imports so far
-import logging
-import os
+# Import the socket module as a named module to help keep our namespace clean
 import socket as s
+import os
+import subprocess
+import time
 
+# Import and configure logging
+import logging
 logging.basicConfig()
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-import PA2_A_Server_Team3
+# DELETE? import PA2_A_Server_Team3
 
 server_host = '10.0.0.1'
-server_port = PA2_A_Server_Team3.SERVER_PORT
+server_port = 12001
 
-# Simple ping test, needs to be modified. 
-ping = os.system("ping -c 10 " + server_host)
-log.info(ping)
+def main():
+  
+  # Create the socket
+  with s.socket(s.AF_INET, s.SOCK_DGRAM) as socket:
+    
+    # Set timeout at 1 second
+    socket.settimeout(1)
+    
+    # List to store RTT values
+    rtt = []
+    
+    # Ping server 10 times
+    for i in range(1, 11):
+      
+      try:
+        ## TODO check if ping should actually be used or client sends message because server code has a message received line in code
+        # Execute ping command and get output
+        ping_output = subprocess.check_output(['ping', '-c', '1', server_host]).decode()
+        
+        # Extract RTT value from ping output
+        for line in ping_output.split('\n'):
+          if 'time=' in line:
+            rtt_str = line.split('time=')[1].split(' ')[0]
+            # Store RTT value
+            rtt.append(float(rtt_str))
+        
+        ## TODO fix print message to match expected output (need sample_rtt, estimated_rtt, dev_rtt)
+        # Print message
+        print('Ping {}: rtt = {} ms'.format(str(i), rtt_str))
+      
+      ## TODO fix timeout because it is not working properly
+      except s.timeout:
+        # Print message
+        print('Ping {}: Request timed out'.format(str(i)))
+    
+    # Print summary values
+    print('Summary values')
+    print('min_rtt = {} ms'.format(min(rtt)))
+    print('max_rtt = {} ms'.format(max(rtt)))
+    mean = sum(rtt) / float(len(rtt))
+    print('ave_rtt = {} ms'.format(mean))
+    print('Packet loss: {}%'.format((10 - len(rtt)) * 10))
+    ## TODO calculate timeout interval
+    print('Timeout Interval: ms')
+      
+  # Close socket    
+  socket.close()
+    
+if __name__ == "__main__":
+  main()
