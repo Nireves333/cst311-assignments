a9dc90db5d1bb2f2b65b3dbaf039dca9920a29b7
Andi <127003684+and1cam@users.noreply.github.com>
2023-05-08 18:06:01 -0700

    Update PA2_A_Client_Team3.py

--- PA2/src/PA2_A_Client_Team3.py
+++ PA2/src/PA2_A_Client_Team3.py
@@ -19,14 +19,13 @@ def main():
   # Create the socket
   with s.socket(s.AF_INET, s.SOCK_DGRAM) as socket:
     
-    # Set timeout at 1 second
-    socket.settimeout(1)
-    
-    # List to store sample_rtt, estimated_rtt, and dev_rtt values
+    # Lists to store RTT values
     sample_rtt = []
     estimated_rtt = []
     dev_rtt = []
     
+    # Set timeout at 1 second
+    socket.settimeout(1)
     # Initialize elapsed time
     elapsed = 0
     
@@ -35,65 +34,56 @@ def main():
       
       # Get start time
       start_time = time.time()
+      # Initialize message
       message = 'Ping {}'.format(str(i))
+      # Send message to server
+      socket.sendto(message.encode(), (server_host, server_port))
       
-      # Try to send message
+      # Try to receive reply
       try:
-        
-        # Send message to server
-        socket.sendto(message.encode(), (server_host, server_port))
-        
-        # If no message received from server
-        if socket.recvfrom(1024) == None:
-          # Print message
-          print('Ping {}: Request timed out'.format(str(i)))
-          # Add sample RTT to list
-          sample_rtt.append(elapsed)
-          # Add estimated RTT to list
-          estimated_rtt.append(elapsed)
-          # Add dev RTT to list
+        reply, _ = socket.recvfrom(1024)
+        # Get end time
+        end_time = time.time()
+        # Calculate elapsed time aka sample RTT
+        elapsed = (end_time - start_time) * 1000
+        # Add sample RTT to list
+        sample_rtt.append(elapsed)
+          
+        # Set initial estimated RTT or calculate current estimated RTT and add to list
+        # Formula from textbook, page ???
+        if len(estimated_rtt) == 0:
+          est_elapsed = elapsed
+          estimated_rtt.append(est_elapsed)
+        else:
+          est_elapsed = (0.875 * est_elapsed) + (0.125 * elapsed)
+          estimated_rtt.append(est_elapsed)
+          
+        # Set initial dev RTT or calculate current dev RTT and add to list
+        # Formula from textbook, page ???
+        if len(dev_rtt) == 0:
           dev_elapsed = elapsed / 2
           dev_rtt.append(dev_elapsed)
-        
-        # Otherwise, record RTT values
         else:
-          
-          # Get end time
-          end_time = time.time()
-          # Calculate elapsed time aka sample RTT
-          elapsed = (end_time - start_time) * 1000
-          # Add sample RTT to list
-          sample_rtt.append(elapsed)
-          
-          ## Does not add up when 1st Ping times out. Unsure if we need to actually fix that, I have a feeling it's the output checkers fault due to it calculating a zero. 
-          # Set initial estimated RTT or calculate current estimated RTT and add to list
-          # Formula from textbook, page ???
-          if len(estimated_rtt) == 0:
-            est_elapsed = elapsed
-            estimated_rtt.append(est_elapsed)
-          else:
-            est_elapsed = (0.875 * est_elapsed) + (0.125 * elapsed)
-            estimated_rtt.append(est_elapsed)
-          
-          ## Does not add up when 1st Ping times out. Unsure if we need to actually fix that, I have a feeling it's the output checkers fault due to it calculating a zero.
-          # Set initial dev RTT or calculate current dev RTT and add to list
-          # Formula from textbook, page ???
-          if len(dev_rtt) == 0:
-            dev_elapsed = elapsed / 2
-            dev_rtt.append(dev_elapsed)
-          else:
-            dev_elapsed = (0.75 * dev_elapsed) + (0.25 * abs(elapsed - est_elapsed))
-            dev_rtt.append(dev_elapsed)
+          dev_elapsed = (0.75 * dev_elapsed) + (0.25 * abs(elapsed - est_elapsed))
+          dev_rtt.append(dev_elapsed)
                
-          # Print message
-          print('Ping {}: sample_rtt = {:.3f} ms, estimated_rtt = {:.3f} ms, dev_rtt = {:.3f} ms'.format(i, elapsed, est_elapsed, dev_elapsed))
-      
-      # Exception happens when timeout is one second or more
+        # Print results
+        print('Ping {}: sample_rtt = {:.3f} ms, estimated_rtt = {:.3f} ms, dev_rtt = {:.3f} ms'.format(i, elapsed, est_elapsed, dev_elapsed))
+        
+      # Throw exception when timeout occurs
       except s.timeout:
-          # Print message
-          print('Ping {}: Request timed out'.format(str(i)))
+        # Print message
+        print('Ping {}: Request timed out'.format(str(i)))
+        # Add sample RTT to list
+        sample_rtt.append(0)
+        # Add estimated RTT to list
+        est_elapsed = elapsed
+        estimated_rtt.append(elapsed)
+        # Add dev RTT to list
+        dev_elapsed = elapsed / 2
+        dev_rtt.append(dev_elapsed)
     
-    # Print summary values header
+    # Print header
     print('Summary values:')
     
     # Print minimum RTT value
@@ -107,9 +97,13 @@ def main():
     print('ave_rtt = {:.3f} ms'.format(mean))
     
     # Print packet loss percentage
-    print('Packet loss: {:.2f}%'.format((10 - len(sample_rtt)) * 10))
+    loss = sample_rtt.count(0) / len(sample_rtt) * 100
+    print('Packet loss: {:.2f}%'.format(loss))
     
     # Calculate timeout interval by using the last index of estimated_rtt and dev_rtt
+    # Initialize final_est and final_dev
+    final_est = 0
+    final_dev = 0
     # Get last float that is not 0 in estimated_rtt list
     for i, est in enumerate(reversed(estimated_rtt)):
       if est > 0:
@@ -131,3 +125,4 @@ def main():
     
 if __name__ == "__main__":
   main()
+  
