c516fa53df25671bbf47dea35e3e820df0cfff24
michelle-brown <56523513+michelle-brown@users.noreply.github.com>
2023-06-08 21:56:37 -0700

    Create legacy_network_MB.py

---
+++ PA4/src/legacy_network_MB.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python
+
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+
+
+
+def myNetwork():
+
+    net = Mininet( topo=None,
+                   build=False,
+                   ipBase='10.0.0.0/24')
+
+    info( '*** Adding controller\n' )
+    c0=net.addController(name='c0',
+                      controller=Controller,
+                      # changed the protocol from 'tcp' to 'tls'
+                      protocol='tls',
+                      port=6633)
+
+    info( '*** Add switches\n')
+    # Moved addSwitch lines for instantiating switches before the routers, and changed order to s1, then s2
+    s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+    s2 = net.addSwitch('s2', cls=OVSKernelSwitch)
+
+    # Re-ordered router instantiation to r3, r4, then r5. Also assigned the IP address of the first node for each router.
+    r3 = net.addHost('r3', cls=Node, ip='10.0.1.1/24')
+    r3.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r4 = net.addHost('r4', cls=Node, ip='192.168.1.2/30')
+    r4.cmd('sysctl -w net.ipv4.ip_forward=1')
+    r5 = net.addHost('r5', cls=Node, ip='192.168.1.6/30')
+    r5.cmd('sysctl -w net.ipv4.ip_forward=1')
+
+    info( '*** Add hosts\n')
+    # Updated IP -
+    # Hosts h1 and h2 and the router r3 interface connected to s1 will be on a
+    # 254-host network with the private IPv4 address of 10.0.x.0/24. (You choose the value of x.)
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.50/24', defaultRoute='via 10.0.1.5')
+    h2 = net.addHost('h2', cls=Host, ip='10.0.1.100/24', defaultRoute='via 10.0.1.1')
+
+    # Hosts h3 and h4 and the router r5 interface connected to s2 will be on a network with the
+    # private IPv4 address of 10.0.y.0/24. (You choose the value of y.)
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.50/24', defaultRoute='via 10.0.2.5')
+    h4 = net.addHost('h4', cls=Host, ip='10.0.2.100/24', defaultRoute='via 10.0.2.1')
+
+    info( '*** Add links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s2)
+    net.addLink(h4, s2)
+
+    # modified links below to add IP addresses for links to routers
+    # net.addLink(s2, r5)
+    # net.addLink(s1, r3)
+    # net.addLink(r3, r4)
+    # net.addLink(r4, r5)
+    # net.addLink(s1, r3, intfName2='r3-eth0', params2={'ip': '10.0.1.1/24'})
+    # net.addLink(s1, r3, intfName2='r3-eth1', params2={'ip': '192.168.1.1/30'})
+    # net.addLink(s2, r5, intfName2='r5-eth0', params2={'ip': '10.0.2.1/24'})
+    # net.addLink(s2, r5, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
+    # net.addLink(r3, r4, intfName1='r3-eth1', params1={'ip': '192.168.1.1/30'})
+    # net.addLink(r3, r4, intfName2='r4-eth0', params2={'ip': '192.168.1.2/30'})
+    # net.addLink(r4, r5, intfName1='r4-eth1', params1={'ip': '192.168.1.5/30'})
+    # net.addLink(r4, r5, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
+
+    # Link between s1 and r3
+    net.addLink(s1, r3, intfName2='r3-eth0', params2={'ip': '10.0.1.1/24'})
+
+    # Link between r3 and r4
+    net.addLink(r3, r4, intfName1='r3-eth1', params1={'ip': '192.168.1.1/30'}, intfName2='r4-eth0', params2={'ip': '192.168.1.2/30'})
+    # Link between r4 and r3
+    # net.addLink(r4, r3, intfName1='r4-eth0', params1={'ip': '192.168.1.2/30'}, intfName2='r3-eth1', params2={'ip': '192.168.1.1/30'})
+
+    # Link between r4 and r5
+    net.addLink(r4, r5, intfName1='r4-eth1', params1={'ip': '192.168.1.5/30'}, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
+    # Link between r5 and r4
+    #net.addLink(r5, r4, intfName1='r4-eth1', params1={'ip': '192.168.1.5/30'}, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
+
+    # Link between s2 and r5
+    net.addLink(s2, r5, intfName2='r5-eth0', params2={'ip': '10.0.2.1/24'})
+
+    info( '*** Starting network\n')
+    net.build()
+    info( '*** Starting controllers\n')
+    for controller in net.controllers:
+        controller.start()
+
+    info( '*** Starting switches\n')
+    net.get('s2').start([c0])
+    net.get('s1').start([c0])
+
+    info( '*** Post configure switches and hosts\n')
+
+    # add static routes for each router
+    # LEFT OFF HERE
+    # Via Router 3
+    # H1 to H2
+    # net["r3"].cmd("ip route add 10.0.1.100/24 via 192.168.1.1 dev r3-eth0")
+    # r5.cmd("ip route add 10.0.1.100/24 via 192.168.1.1 dev r5-eth1")
+    # r3.cmd("ip route add 10.0.2.100/24 via 192.168.1.6 dev r3-eth1")
+    r3.cmd('route add -net 10.0.1.50/24 gw 10.0.1.1 dev r3-eth0')
+    r3.cmd('route add -net 10.0.1.100/24 gw 10.0.1.1 dev r3-eth0')
+
+    CLI(net)
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    myNetwork()
