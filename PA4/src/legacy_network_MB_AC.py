cb81af73e68bd406b350b842d0e34420c313e615
Andi <127003684+and1cam@users.noreply.github.com>
2023-06-08 23:10:17 -0700

    Updated and renamed file
    See comments

--- PA4/src/legacy_network_MB_AC.py
+++ PA4/src/legacy_network_MB_AC.py
@@ -1,4 +1,11 @@
-#!/usr/bin/python
+"""Legacy network for CST311 Programming Assignment 4"""
+__author__ = "Team 3"
+__credits__ = [
+  "Andi Cameron",
+  "Michelle Brown",
+  "Nathan Simpson",
+  "Severin Light"
+]
 
 from mininet.net import Mininet
 from mininet.node import Controller, RemoteController, OVSController
@@ -10,9 +17,6 @@ from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
 from subprocess import call
 
-
-
-
 def myNetwork():
 
     net = Mininet( topo=None,
@@ -23,7 +27,8 @@ def myNetwork():
     c0=net.addController(name='c0',
                       controller=Controller,
                       # changed the protocol from 'tcp' to 'tls'
-                      protocol='tls',
+                      # DELETE COMMENT LATER - ANDI CHANGED - tls -> tcp
+                      protocol='tcp',
                       port=6633)
 
     info( '*** Add switches\n')
@@ -36,19 +41,24 @@ def myNetwork():
     r3.cmd('sysctl -w net.ipv4.ip_forward=1')
     r4 = net.addHost('r4', cls=Node, ip='192.168.1.2/30')
     r4.cmd('sysctl -w net.ipv4.ip_forward=1')
-    r5 = net.addHost('r5', cls=Node, ip='192.168.1.6/30')
+    # DELETE COMMENT LATER - ANDI CHANGED - 30 -> 24
+    # DELETE COMMENT LATER - ANDI QUESTION - should ip = 10.0.2.1?
+    r5 = net.addHost('r5', cls=Node, ip='192.168.1.6/24')
     r5.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     info( '*** Add hosts\n')
     # Updated IP -
     # Hosts h1 and h2 and the router r3 interface connected to s1 will be on a
     # 254-host network with the private IPv4 address of 10.0.x.0/24. (You choose the value of x.)
-    h1 = net.addHost('h1', cls=Host, ip='10.0.1.50/24', defaultRoute='via 10.0.1.5')
+    # DELETE COMMENT LATER - ANDI CHANGED - 5 -> 1
+    h1 = net.addHost('h1', cls=Host, ip='10.0.1.50/24', defaultRoute='via 10.0.1.1')
+    # DELETE COMMENT LATER - ANDI QUESTION - should default route be 10.0.2.1?
     h2 = net.addHost('h2', cls=Host, ip='10.0.1.100/24', defaultRoute='via 10.0.1.1')
 
     # Hosts h3 and h4 and the router r5 interface connected to s2 will be on a network with the
     # private IPv4 address of 10.0.y.0/24. (You choose the value of y.)
-    h3 = net.addHost('h3', cls=Host, ip='10.0.2.50/24', defaultRoute='via 10.0.2.5')
+    # DELETE COMMENT LATER - ANDI CHANGED - 5 -> 1
+    h3 = net.addHost('h3', cls=Host, ip='10.0.2.50/24', defaultRoute='via 10.0.2.1')
     h4 = net.addHost('h4', cls=Host, ip='10.0.2.100/24', defaultRoute='via 10.0.2.1')
 
     info( '*** Add links\n')
@@ -72,7 +82,8 @@ def myNetwork():
     # net.addLink(r4, r5, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
 
     # Link between s1 and r3
-    net.addLink(s1, r3, intfName2='r3-eth0', params2={'ip': '10.0.1.1/24'})
+    # DELETE LATER - ANDI CHANGED - OG = (s1, r3, intfName2='r3-eth0', params2={'ip': '10.0.1.1/24'})
+    net.addLink(s1, r3)
 
     # Link between r3 and r4
     net.addLink(r3, r4, intfName1='r3-eth1', params1={'ip': '192.168.1.1/30'}, intfName2='r4-eth0', params2={'ip': '192.168.1.2/30'})
@@ -85,7 +96,8 @@ def myNetwork():
     #net.addLink(r5, r4, intfName1='r4-eth1', params1={'ip': '192.168.1.5/30'}, intfName2='r5-eth1', params2={'ip': '192.168.1.6/30'})
 
     # Link between s2 and r5
-    net.addLink(s2, r5, intfName2='r5-eth0', params2={'ip': '10.0.2.1/24'})
+    # DELETE COMMENT LATER - ANDI CHANGED - OG = (s2, r5, intfName2='r5-eth0', params2={'ip': '10.0.2.1/24'})
+    net.addLink(s2, r5)
 
     info( '*** Starting network\n')
     net.build()
@@ -106,8 +118,29 @@ def myNetwork():
     # net["r3"].cmd("ip route add 10.0.1.100/24 via 192.168.1.1 dev r3-eth0")
     # r5.cmd("ip route add 10.0.1.100/24 via 192.168.1.1 dev r5-eth1")
     # r3.cmd("ip route add 10.0.2.100/24 via 192.168.1.6 dev r3-eth1")
-    r3.cmd('route add -net 10.0.1.50/24 gw 10.0.1.1 dev r3-eth0')
-    r3.cmd('route add -net 10.0.1.100/24 gw 10.0.1.1 dev r3-eth0')
+    # r3.cmd('route add -net 10.0.1.50/24 gw 10.0.1.1 dev r3-eth0')
+    # r3.cmd('route add -net 10.0.1.100/24 gw 10.0.1.1 dev r3-eth0')
+    
+    # DELETE COMMENT LATER - ANDI ADDED
+    r3.cmd('ip route add 10.0.1.0/24 via 192.168.1.2 dev r3-eth1')
+    r3.cmd('ip route add 192.168.1.3/30 via 192.168.1.2 dev r3-eth1')
+
+    # DELETE COMMENT LATER - ANDI ADDED
+    r4.cmd('ip route add 10.0.1.0/24 via 192.168.1.1 dev r4-eth0')
+    r4.cmd('ip route add 10.0.2.0/24 via 192.168.1.6 dev r4-eth1')
+  
+    # DELETE COMMENT LATER - ANDI ADDED
+    r5.cmd('ip route add 10.0.2.0/24 via 192.168.1.5 dev r5-eth1')
+    r5.cmd('ip route add 192.168.1.4/30 via 192.168.1.5 dev r5-eth1')
+    
+    # DELETE COMMENT LATER - ANDI ADDED - Code to run other parts (I think)
+    # Create and issue certificate
+    # h2.cmd('python3 /home/mininet/CST311/???.py')
+    # Start h1, h2, h3, h4 terminals
+    # makeTerm(h1, title='Client host 1', term='xterm', display=None, cmd='python3 /home/mininet/CST311/???.py')
+    # makeTerm(h2, title='TLS-enabled webserver', term='xterm', display=None, cmd='python3 /home/mininet/CST311/???.py')
+    # makeTerm(h3, title='Client host 2', term='xterm', display=None, cmd='python3 /home/mininet/CST311/???.py')
+    # makeTerm(h4, title='TLS-enabled chat server', term='xterm', display=None, cmd='python3 /home/mininet/CST311/???.py')
 
     CLI(net)
     net.stop()
