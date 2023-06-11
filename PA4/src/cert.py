e75f1813db31a0d7f5320c237d9d079051636afc
Nathan S <115956049+Br-ak@users.noreply.github.com>
2023-06-11 16:56:01 -0700

    Add files via upload
    Cert.py 
    Changed beginning to keep directories if they already exist, so that other certs/keys don't get deleted.
    Added 'hostIP', 'CN', and 'CNbase' variables to let the server determine real-time.
    Changed calls to reflect these new variables.
    
    pa4webserver.py
    Now asks the user for input for desired CN (Common Name) to be used.
    Used user-created CN for server address and reflects new CN in 'ssl_key_file' and 'ssl_certificate_file' file paths.

--- PA4/src/cert.py
+++ PA4/src/cert.py
@@ -9,19 +9,15 @@ __credits__ = [
 
 import subprocess
 import os
+import sys
 
-# Check if folders for certificates exist, if so delete
-if os.path.exists("/home/mininet/CST311/certs"):
-    subprocess.call(['sudo', 'rm', '-rf', '/home/mininet/CST311/certs'])
-if os.path.exists("/home/mininet/CST311/newcerts"):
-    subprocess.call(['sudo', 'rm', '-rf', '/home/mininet/CST311/newcerts'])
-if os.path.exists("/home/mininet/CST311/private"):
-    subprocess.call(['sudo', 'rm', '-rf', '/home/mininet/CST311/private'])
-
-# Create directory
-os.mkdir('/home/mininet/CST311/certs')
-os.mkdir('/home/mininet/CST311/newcerts')
-os.mkdir('/home/mininet/CST311/private')
+# Check if folders for certificates exist, otherwise create them
+if not os.path.exists("/home/mininet/CST311/certs"):
+  os.mkdir('/home/mininet/CST311/certs')
+if not os.path.exists("/home/mininet/CST311/newcerts"):
+  os.mkdir('/home/mininet/CST311/newcerts')
+if not os.path.exists("/home/mininet/CST311/private"):
+  os.mkdir('/home/mininet/CST311/private')
 
 # Working directory
 working_dir = '/home/mininet/CST311/certs'
@@ -41,25 +37,47 @@ subprocess.call(['sudo', 'cp', 'cacert.pem', '/usr/local/share/ca-certificates/c
 # Run ca-certificates application
 subprocess.call(['sudo', 'update-ca-certificates'])
 
+# Retrieve the passed variables
+CN = sys.argv[1]
+hostIP = sys.argv[2]
+
+# Convert input to remove www. and .test for key/cert file names
+# Remove "www." prefix
+if CN.startswith("www."):
+  CNbase = CN[4:]
+        
+# Remove ".test" suffix
+if CNbase.endswith(".test"):
+  CNbase = CNbase[:-5]
+
 # Generate private key for server
-subprocess.call(['sudo', 'openssl', 'genrsa', '-out', 'webpa4.test-key.pem', '-passout', 'pass:1234', '2048'], cwd=working_dir)
+subprocess.call(['sudo', 'openssl', 'genrsa', '-out', CNbase + '-key.pem', '-passout', 'pass:1234', '2048'], cwd=working_dir)
 
 # Generate certificate signing request using server private key
-subprocess.call(['sudo', 'openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', 'webpa4.test-key.pem', '-out', 'webpa4.test.csr', '-subj', '/C=US/ST=CA/L=Monterey/O=CST311/OU=Networking/CN=www.webpa4.test'], cwd=working_dir)
+subprocess.call(['sudo', 'openssl', 'req', '-nodes', '-new', '-config', '/etc/ssl/openssl.cnf', '-key', CNbase + '-key.pem', '-out', CNbase + '.csr', '-subj', '/C=US/ST=CA/L=Monterey/O=CST311/OU=Networking/CN=' + CN], cwd=working_dir)
 
 # Create server certificate
-subprocess.call(['sudo', 'openssl', 'x509', '-req', '-days', '365', '-in', 'webpa4.test.csr', '-CA', 'cacert.pem', '-CAkey', './private', '-CAcreateserial', '-out', 'webpa4.test-cert.pem', '-passin', 'pass:1234'], cwd=working_dir)
+subprocess.call(['sudo', 'openssl', 'x509', '-req', '-days', '365', '-in', CNbase + '.csr', '-CA', 'cacert.pem', '-CAkey', './private', '-CAcreateserial', '-out', CNbase + '-cert.pem', '-passin', 'pass:1234'], cwd=working_dir)
 
 # Copy certificate and key into subdirectories
-subprocess.call(['sudo', 'mv', './webpa4.test-cert.pem', './newcerts'], cwd=working_dir)
-subprocess.call(['sudo', 'mv', './webpa4.test-key.pem', './private'], cwd=working_dir)
+subprocess.call(['sudo', 'mv', './' + CNbase + '-cert.pem', "/home/mininet/CST311/newcerts/"], cwd=working_dir)
+subprocess.call(['sudo', 'mv', './' + CNbase + '-key.pem', "/home/mininet/CST311/private/"], cwd=working_dir)
 
 # Protect server’s private key
 subprocess.call(['sudo', 'chmod', '-R', '600', 'private'], cwd=working_dir) 
 
 # Modify host file to include IP addresses
-subprocess.call(['sudo', 'sh', '-c', 'echo \"127.0.0.1\ca.pa4.test\" >> /etc/hosts'])
-subprocess.call(['sudo', 'sh', '-c', 'echo \"10.0.1.100\twww.webpa4.test\" >> /etc/hosts'])
+subprocess.call(['sudo', 'sh', '-c', 'echo "127.0.0.1\tca.pa4.test" >> /etc/hosts'])
+subprocess.call(['sudo', 'sh', '-c', 'echo "' + hostIP + '\t' + CN + '" >> /etc/hosts'])
+
+# Move cacert.pem to ssl/certs folder
+file_path = '/home/mininet/CST311/cacert.pem'
+
+if os.path.exists(file_path):
+  subprocess.call (['sudo', 'mv' , "/home/mininet/CST311/cacert.pem" , "/etc/ssl/certs/"])
+
+# Run ca-certificates application
+subprocess.call(['sudo', 'update-ca-certificates'])
 
 # Print success message
 print('***Server Certificate Issued***')
---
+++ PA4/src/pa4webserver.py
@@ -0,0 +1,46 @@
+"""TLS webserver for CST311 Programming Assignment 4"""
+__author__ = "Team 3"
+__credits__ = [
+  "Andi Cameron",
+  "Michelle Brown",
+  "Nathan Simpson",
+  "Severin Light"
+]
+
+import http.server
+import ssl
+import subprocess
+import os
+
+# Set variables used by cert.py before running
+H2IP = "10.0.1.100"
+CN = input("Enter desired Common Name (CN) [format www.example.test]: ")
+# Generate CERTS/KEYS
+subprocess.run(['python', "/home/mininet/CST311/Assignment4/cert.py", CN, H2IP])
+
+# Convert input to remove www. and .com for key/cert file names
+# Remove "www." prefix
+if CN.startswith("www."):
+  CNbase = CN[4:]
+        
+# Remove ".test" suffix
+if CNbase.endswith(".test"):
+  CNbase = CNbase[:-5]
+  
+# Variables, including location of server certificate and private key file
+server_address = "www." + CNbase + ".test"
+server_port = 4443
+ssl_key_file = os.path.join("/home/mininet/CST311/private/", CNbase + "-key.pem")
+ssl_certificate_file = os.path.join("/home/mininet/CST311/newcerts/", CNbase + "-cert.pem")
+
+#Context is the TLS Server with its certificate file and key file location
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(ssl_certificate_file, ssl_key_file)
+ 
+## Don't modify anything below
+httpd = http.server.HTTPServer((server_address, server_port), http.server.SimpleHTTPRequestHandler)
+httpd.socket = context.wrap_socket(httpd.socket,
+               server_side=True)
+
+print("Listening on port", server_port)                                
+httpd.serve_forever()
